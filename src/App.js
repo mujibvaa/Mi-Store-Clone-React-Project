73b28cde003673b209cf478c1f9a80c983d7fe86
Mujib Ahamad <mujibvaaa@gmail.com>
2022-06-14 00:47:51 +0600

    prenav,nav,slider done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import PreNavBar from './components/PreNavBar';
+import NavBar from "./components/NavBar.js";
+import { BrowserRouter as Router } from "react-router-dom"
+import Slider from "./components/Slider.js"
+import data from "./data/data.json"
+import Offers from "./components/Offers.js"
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <PreNavBar/>
+      <NavBar/> 
+      <Slider start={data.banner.start}/>
+      <Offers offer={data.offer}/>
+ 
+    </Router>
+   
+
   );
 }
 
